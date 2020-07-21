7534d112b8ab5d31bba0b691f91616caf85cfc00
MavoSV8 <Lukaszpasi@gmail.com>
2020-07-21 22:44:30 +0200

    Dodane klasy

---
+++ src/myprojects/proj1/Contact.java
@@ -0,0 +1,26 @@
+package myprojects.proj1;
+
+public class Contact {
+    String phoneNum;
+    String firstName;
+    String lastName;
+
+    public Contact(String num,String fName,String lName)
+    {
+
+
+    }
+
+    public void setFirstName(String firstName) {
+        firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        lastName = lastName;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        phoneNum = phoneNum;
+    }
+
+}
---
+++ src/myprojects/proj1/MobilePhone.java
@@ -0,0 +1,10 @@
+package myprojects.proj1;
+
+import java.util.ArrayList;
+
+public class MobilePhone
+{
+    ArrayList<Contact> Contacts = new ArrayList<Contact>();
+
+
+}
