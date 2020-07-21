e34fe93b52a7d475ec058cf7b071dbf55b7f416a
MavoSV8 <Lukaszpasi@gmail.com>
2020-07-21 23:11:32 +0200

    Zaktualizowane klasy

--- src/myprojects/proj1/MobilePhone.java
+++ src/myprojects/proj1/MobilePhone.java
@@ -4,7 +4,31 @@ import java.util.ArrayList;
 
 public class MobilePhone
 {
-    ArrayList<Contact> Contacts = new ArrayList<Contact>();
+    ArrayList<Contact> Contacts = new ArrayList<>();
+    public void addContact(String phoneNr,String fName,String lName)
+    {
+    Contact
 
+
+    }
+    public void removeContact(String name)
+    {
+
+
+    }
+    public void listContacts()
+    {
+
+
+    }
+    public void updateContact(Contact contact)
+    {
+
+    }
+    public void searchContact(String name)
+    {
+
+
+    }
 
 }
